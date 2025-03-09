1c6e5d81681a420d58a464c6901d357b487bd60f
Phillip Wood <Phillipwood2023@outlook.com>
2025-03-09 11:54:17 -0400

    Player Shoot rate

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(circleshape.CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shoot_timer = 0
 
     # in the player class
     def triangle(self):
@@ -21,6 +22,9 @@ class Player(circleshape.CircleShape):
         pygame.draw.polygon(screen, pygame.color.Color("white"), self.triangle(), 2)
 
     def update(self, dt):
+        if self.shoot_timer > 0:
+            self.shoot_timer -= dt
+
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -42,5 +46,6 @@ class Player(circleshape.CircleShape):
         self.rotation += (PLAYER_TURN_SPEED * (dt * direction))
     
     def shoot(self):
-        projectile.Projectile(self.position.x, self.position.y, self.rotation)
-        
+        if self.shoot_timer <= 0:
+            self.shoot_timer = PLAYER_SHOOT_COOLDOWN
+            projectile.Projectile(self.position.x, self.position.y, self.rotation)
